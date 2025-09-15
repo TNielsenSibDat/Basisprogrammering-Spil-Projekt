1a675b1b68ec5ccc036d1ac2d4f7e542284f91d4
AlanGharati <alaagharatiii@gmail.com>
2025-09-15 09:57:49 +0200

    Oprettet kode til menu 1 del

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,11 +14,51 @@ namespace Basisprogrammering_Spil_Projekt
         {
             //Kode til menu
 
+            Console.WriteLine("Velkommen til!");
+            
+
+            Console.WriteLine("Tryk 1 for det ene spil");
+            Console.WriteLine("Tryk 2 for det andet spil");
+            Console.WriteLine("Tryk Quit for at afslutte spillet");
+            string spillerValg = Console.ReadLine();
+
+            switch (spillerValg)
+            {
+                case "1":
+                    GætTallet();
+                    break;
+                    
+
+                case "2":
+                    break;
+
+                case "Quit":
+                    break;
+            
+
+                
+                }
+
+            
+
+
 
             //Funktioner til første spil
 
 
             //Funktioner til andet spil
+     
+        
+      
+            
+       }
+        private static void GætTallet()
+        {
+      
         }
-    }
+    } 
+ 
 }
+
+
+
